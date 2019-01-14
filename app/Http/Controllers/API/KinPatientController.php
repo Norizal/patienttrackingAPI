0e75ef6c3a5f726bba5bd384caf4a8506b7d7341
Norizal <norizal.rizal4@gmail.com>
2019-01-14 11:08:58 +0800

    Eightteen Commit

--- app/Http/Controllers/API/KinPatientController.php
+++ app/Http/Controllers/API/KinPatientController.php
@@ -23,16 +23,47 @@ class KinPatientController extends Controller
     public function getPatient(){
 
         $user = Auth::user()->id;
-        $data = Kin::where('user_id',  $user)->first();
-        $userKin = $data->kin_id;
+        $dataKin = Kin::where('user_id',  $user)->first();
+        $userKin = $dataKin->kin_id;
+
+        $dataPatient = Patient::where('kin_id',  $userKin)->first();
+
+        $userPatient = $dataPatient->hukm_id;
+        $userLocation = $dataPatient->location_id;
+        $userBeacon = $dataPatient->beacon_id;
+        $userMedicalStatus = $dataPatient->medical_status_id;
+
+        $dataHUKM = PatientHUKM::where('hukm_id',  $userPatient)->first();
+
+        $data = $dataHUKM->hukm_icnumber;
+        $data = $dataHUKM->hukm_name;
+        $data = $dataHUKM->hukm_mrn;
+        $data = $dataHUKM->hukm_gender;
+        $data = $dataHUKM->hukm_age;
+        $data = $dataHUKM->hukm_race;
+        $data = $dataHUKM->hukm_phonenumber;
+
+        $dataLocation = Location::where('location_id',  $userLocation)->first();
+
+        $data = $dataLocation->location_name;
+
+        $dataBeacon = Beacon::where('beacon_id',  $userBeacon)->first();
+
+        $data = $dataBeacon->beacon_name;
+
+        $dataMedicalStatus = MedicalStatus::where('medical_status_id',  $userMedicalStatus)->first();
+
+        $data = $dataMedicalStatus->medical_status_name;
+
+
 
       
-        $data = DB::table('patient')
-        ->select('patient_hukm.hukm_name','patient_hukm.hukm_icnumber','patient_hukm.hukm_mrn','patient_hukm.hukm_gender','patient_hukm.hukm_age', 'patient_hukm.hukm_race', 'patient_hukm.hukm_phonenumber', 'location.location_name')
-        ->join('patient_hukm', 'patient_hukm.hukm_id', '=', 'patient.hukm_id')
-        ->join('location', 'location.location_id', '=', 'patient.location_id')
-        ->where('patient.kin_id', $userKin)
-        ->get();
+        // $data = DB::table('patient')
+        // ->select('patient_hukm.hukm_name','patient_hukm.hukm_icnumber','patient_hukm.hukm_mrn','patient_hukm.hukm_gender','patient_hukm.hukm_age', 'patient_hukm.hukm_race', 'patient_hukm.hukm_phonenumber', 'location.location_name')
+        // ->join('patient_hukm', 'patient_hukm.hukm_id', '=', 'patient.hukm_id')
+        // ->join('location', 'location.location_id', '=', 'patient.location_id')
+        // ->where('patient.kin_id', $userKin)
+        // ->get();
         
         
         
